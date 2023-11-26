189b4e2f217e48d2a90cd8a712ee330fe49a41c8
Anikit <chakrabortyanikit115@gmail.com>
2023-11-26 13:04:59 +0530

    First assignment

---
+++ mario.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+   do{
+     n= get_int("Height:\n");
+   }
+   while(n<1 || n>8);
+
+    for(int i=1;i<=n;i++ ){
+
+    for(int j = n-i;j>=0;j--){
+    printf(" ");
+    }
+   for(int k =0;k<i;k++){
+    printf("#");
+   }
+   printf("\n");
+   }
+
+
+
+
+
+}
